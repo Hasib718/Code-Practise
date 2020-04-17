ba89dfeb137baef46f1f16b4519026fe6b1b946b
Hasib718 <smalreal14@gmail.com>
2020-04-18 01:06:42 +0600

    One unsolved python problem
    1337B.py have some sort of bug

--- Java/Again.java
+++ Java/Again.java
@@ -4,9 +4,11 @@ public class Again {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        String str = "Fucking Github";
+
         int a = in.nextInt();
         int b = in.nextInt();
 
-        System.out.println(a+b);
+        System.out.println(a+b+str);
     }
 }
